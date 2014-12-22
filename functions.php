23e56444a97dfa08be5bb5134e0406526a0533e4
Panayiotis Georgiou <info@panayiotisgeorgiou.net>
2014-12-22 13:36:23 +0200

    if statement refinement

--- functions.php
+++ functions.php
@@ -1,13 +1,11 @@
+<?php
 function custom_cf7_required_radio_filter($result, $tag) {
     $name = $tag['name'];
-    if (
-        $tag['type'] == 'radio'
-        //&& substr($name, -9) == '-required'
-        && empty($_POST[$name])
-    ) {
+    if ( $tag['type'] == 'radio' && empty($_POST[$name])) {
         $result['valid'] = false;
         $result['reason'][$name] = 'Please fill in this required field';
     }
     return $result;
 }
 add_filter('wpcf7_validate_radio', 'custom_cf7_required_radio_filter', 10, 2);
+?>
